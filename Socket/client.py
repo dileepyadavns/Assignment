97f639ce95558c6e2651bdbda83960fe15ef7499
dileepyadavns <102239765+dileepyadavns@users.noreply.github.com>
2022-04-15 18:06:17 +0530

    Update client.py

--- Socket/client.py
+++ Socket/client.py
@@ -1,11 +1,11 @@
-from socket import *
-c=socket()
-c.connect(('localhost',12111))
+from socket import * #to import socket
+c=socket() #created an instance for socket
+c.connect(('localhost',12111)) #to connect to the port
 print("connect to server")
 while True:
     recData=c.recv(1024).decode()
     print('Server: ',recData)
-    data=input("Client : ")
+    data=input("Client : ") #to send message 
     c.send(bytes(data ,'utf-8'))
 
-c.close()
+c.close() #closing the connection
