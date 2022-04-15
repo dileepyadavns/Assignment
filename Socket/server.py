fccea26625b465d9e8e937201bba6d833ec090f0
dileepyadavns <102239765+dileepyadavns@users.noreply.github.com>
2022-04-15 18:08:46 +0530

    Update server.py

--- Socket/server.py
+++ Socket/server.py
@@ -1,3 +1,4 @@
+#im porting modules
 from socket import *
 from time import *
 
@@ -10,10 +11,10 @@ connection,address = server.accept()
 print("connected to client")
 
 while True:
-    data=input('Server: ')
+    data=input('Server: ') #to send message
     connection.send(bytes(data+ctime(),'utf-8'))
 
-    recData=connection.recv(1024).decode()
-    print('Client: '+recData)
+    recData=connection.recv(1024).decode() #decoded the message from client
+    print('Client: '+recData) 
 
-connection.close()
+connection.close() #closing the connection
