6bca9322464a83b6f0b39d2cb8a62618b0a93f23
dileepyadavns <102239765+dileepyadavns@users.noreply.github.com>
2022-04-16 20:43:33 +0530

    Update project.py

--- ComputerVision/project.py
+++ ComputerVision/project.py
@@ -1,19 +1,19 @@
 #Computer Vision 
 
-#!git clone https://github.com/ultralytics/yolov5   #clone yolov5 git repo before implemnting the file
+!git clone https://github.com/ultralytics/yolov5   #clone yolov5 git repo before implemnting the file
 
-#%cd yolov5
-#%pip install -qr requirements.txt
+%cd yolov5
+%pip install -qr requirements.txt
 
-#importing pytorch.
+importing pytorch.
 import torch
 from yolov5 import utils
 display = utils.notebook_init().
 
 #Unziping the training data.
-#!unzip -q ../train_data.zip -d../ 
+!unzip -q ../train_data.zip -d../ 
 
-#Train YOLOv5s on COCO128 for 3 epochs
+#Train YOLOv5s on COCO128 for 39 epochs
 !python train.py --img 640 --batch 16 --epochs 39 --data coco128.yaml --weights yolov5s.pt --cache
 
 #Predicting
