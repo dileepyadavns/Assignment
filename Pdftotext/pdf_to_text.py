58a3409a3ddc0363d1c33a7df79aab14db8053cc
dileepyadavns <dileepyadavneo@gmail.com>
2022-04-28 12:35:22 +0530

    done crud operations

--- Pdftotext/pdf_to_text.py
+++ Pdftotext/pdf_to_text.py
@@ -2,7 +2,7 @@
 
 import PyPDF2 #imported library
 
-pdfFileObj = open('sample.pdf', 'rb') #given a pdf here as argument
+pdfFileObj = open('Pdftotext/sample.pdf', 'rb') #given a pdf here as argument
 
 print(pdfFileObj)
 
@@ -12,27 +12,28 @@ n=pdfReader.numPages #number of pages in pdf
 
 print(n)
 
-textfile= open('textfile.txt','w') #created a new text file to write contents from pdf after reading
+textfile= open('textfile.txt','a') #created a new text file to write contents from pdf after reading
 
 #used for loop to iterrate over all the pages of pdf
 for i in range(n):
   
   content=pdfReader.getPage(i).extractText() #read content from each page
-  print(content)
+  
   textfile.write(content)
   textfile.write('\n') #written into text file
 
 file=open('textfile.txt','r') # to open text file
-
+file.seek(0)
 data = file.read() # to read text file
 
 #function to search whether particular word in text file or not
-def seaching_Word(sw): 
-  if sw in data:
+def searching_Word(sw): 
+  if sw in data.split():
+    
     occurrences = data.count(sw)
     return occurrences
   else:
     return "Not found matched word"
 
-sw=input("Enter the word to search")
-print(seaching_Word(sw))
+sw=input("Enter the word to search ")
+print("The Number of times the word {} in text file is: ".format(sw) + str(searching_Word(sw)))
