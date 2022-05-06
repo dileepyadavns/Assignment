7eb1baf7a3b053a160e2c936ce16e4ed03339788
dileepyadavns <dileepyadavneo@gmail.com>
2022-05-06 19:15:01 +0530

    added the unique idea feature to database if email exists alerday and your trying add same email it will pop error

--- Ajax_assingment/app.py
+++ Ajax_assingment/app.py
@@ -1,9 +1,11 @@
 import json
-import os
+
+
 from flask import Flask, render_template, request, redirect, url_for, flash
 import psycopg2 #pip install psycopg2 
 import psycopg2.extras
-from flask_sqlalchemy import SQLAlchemy 
+
+
 
 conn = psycopg2.connect( #psycopg2 database adaptor for implementing python
         host="localhost",
@@ -17,7 +19,7 @@ app = Flask(__name__)
 def rowdic():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
    
-    s = "SELECT * FROM students"
+    s = "SELECT * FROM student_details"
     cur.execute(s)
     res = cur.fetchall()
     list_rows = json.dumps([dict(r) for r in res])
@@ -32,9 +34,10 @@ def Index():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)  #cursor is used to read the rows in the table 
     #A cursor that keeps a list of column name is DictCursor
 
-    s = "SELECT * FROM students"
+    s = "SELECT * FROM student_details"
     cur.execute(s) # Execute the SQL
     list_users = cur.fetchall()#fetches a
+    conn.commit()
     print(list_users)
     return render_template('index.html', list_users = list_users)
 
@@ -48,51 +51,77 @@ def add_student():
         fname = request.form['fname']
         lname = request.form['lname']
         email = request.form['email']
+        try:
+            cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+            cur.execute("""INSERT INTO student_details (fname, lname, email) VALUES
+                (%s, %s,%s)""",(fname,lname,email))        
+            conn.commit()
+            
+            #return render_template("index.html", msg="email exists")
+        except psycopg2.IntegrityError:
+            conn.rollback()
+            s = "SELECT * FROM student_details"
+            cur.execute(s) # Execute the SQL
+            list_users = cur.fetchall()#fetches a
+            conn.commit()
+            print(list_users)
+            return render_template('index.html', list_users = list_users,msg='A user exists with same email try with new email')
+    return redirect(url_for('Index'))
+    
        
-        cur.execute("INSERT INTO students (fname, lname, email) VALUES (%s,%s,%s)", (fname, lname, email))
-        conn.commit()
-        flash('Student Added successfully')
-        return redirect(url_for('Index'))
-
-
 #to edit the conetents of table
 @app.route('/edit/<id>', methods = ['POST', 'GET'])
 def get_employee(id):
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
     # DictCursor is a Cursor class that returns rows as dictionaries and stores the result set in the client
    
-    cur.execute('SELECT * FROM students WHERE id = %s', [id])
+    cur.execute('SELECT * FROM student_details WHERE id = %s', [id])
     data = cur.fetchall()#all rows of table are fetched and returned as list of tuples 
     cur.close()
    
     return render_template('edit.html', student = data[0])
 
 #update an row in the table
-@app.route('/update/<id>', methods=['POST'])
+@app.route('/update/<id>', methods=['GET','POST'])
 def update_student(id):
     if request.method == 'POST':
         fname = request.form['fname']
         lname = request.form['lname']
         email = request.form['email']
-         
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute("""
-            UPDATE students
+
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+
+        try:
+            cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+            cur.execute("""
+            UPDATE student_details
             SET fname = %s,
-                lname = %s,
-                email = %s
-            WHERE id = %s
-        """, (fname, lname, email, id))
-        flash('Student Updated Successfully')
-        conn.commit()
-        return redirect(url_for('Index'))        
+            lname = %s,
+            email = %s
+            WHERE id = %s""", (fname, lname, email, id))
+
+            
+        except psycopg2.IntegrityError:
+            conn.rollback()
+            s = "SELECT * FROM student_details"
+            cur.execute(s) # Execute the SQL
+            list_users = cur.fetchall()#fetches a
+            conn.commit()
+            print(list_users)
+            
+            
+            return render_template('index.html', list_users = list_users,msg='A user exists with same email try with new email')
+    return redirect(url_for('Index'))
+    
+       
+              
  
 #To delete the row from table
 @app.route('/delete/<string:id>', methods = ['POST','GET'])
 def delete_student(id):
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
    
-    cur.execute('DELETE FROM students WHERE id = {0}'.format(id))
+    cur.execute('DELETE FROM student_details WHERE id = {0}'.format(id))
     conn.commit()
     flash('Student Removed Successfully')
     return redirect(url_for('Index')) 
@@ -100,5 +129,5 @@ def delete_student(id):
 app.secret_key = 'the random string' 
 if __name__ == "__main__":
    
-    app.run(debug=True,port=5600)
+    app.run(debug=True,port=5900)
    
